42c47e9ac6549068d4b0b79b4ca08ef268cfaac2
Dan G <daniel.w.gray1994@gmail.com>
2023-05-29 18:06:10 -0400

    added text content to all remaining fields using IDs on each element

--- assets/script.js
+++ assets/script.js
@@ -40,10 +40,17 @@ function getWeatherApi () {
         .then(function (data) {
             var dayElIndex = 1
             console.log(data)
+            // i is set to 5 because this API give us data every 3 hours
+            // 5 is the first noon entry in the data set
+            // i+=8 is the final parameter for our loop because 8 x 3 gives us our full 24 hour day
             for (i = 5; i < 39; i+=8) {
                 document.getElementById("day-"+ dayElIndex + "-date").textContent = data.list[i].dt_txt.slice(0, 10);
                 document.getElementById("day-"+ dayElIndex + "-temp").textContent = "Temp: " + data.list[i].main.temp + " F\xB0"
+                document.getElementById("day-"+ dayElIndex + "-wind").textContent = "Wind: " + data.list[i].wind.speed + " MPH"
+                document.getElementById("day-"+ dayElIndex + "-wind").textContent = "Wind: " + data.list[i].wind.speed + " MPH"
+                document.getElementById("day-"+ dayElIndex + "-humid").textContent = "Humidity: " + data.list[i].main.humidity + "%"
 
+                dayElIndex++;
             }
           })
       })
